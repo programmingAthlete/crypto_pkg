f75fbb085cda60b65448f7648aac17259087e73d
Luca <44218525+programmingAthlete@users.noreply.github.com>
2022-10-25 21:30:37 +0200

    Testing (#11)
    * testing
    
    * test rsa
    
    * tests

---
+++ tests/test_rsa.py
@@ -0,0 +1,15 @@
+import unittest
+
+from crypto_pkg.rsa.rsa_scheme import RSA
+
+
+class TestRSA(unittest.TestCase):
+
+    def test_rsa(self):
+        rsa = RSA(k=100)
+        primes = rsa.generate_primes()
+        message = 'hello'
+        keys = rsa.generate_keys(primes.q, primes.p)
+        c = RSA.encrypt_message(message=message, e=keys.public, n=keys.modulus)
+        m = RSA.decrypt_message(cipher_text=c, d=keys.private, n=keys.modulus)
+        self.assertEqual(message, m)
